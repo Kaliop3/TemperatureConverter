0c44aafa4f7cb5ddf2892ca3f3180cf0b713c537
Klaudia Lasoń <klaudiaklason@gmail.com>
2018-01-19 09:44:21 +0100

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const rs = require("readline-sync");
 let change = rs.question("Co chcesz zmienić\nC-celcjusze na fahrenheit\nF-fahrenheit na celcjusze ").toUpperCase();
 let number = rs.question("Jaką liczbę zanienić ? ");
+
 if (change == "F") {
     number = (number - 32) * 5 / 9;
     if (number < -273.15) {
@@ -9,7 +10,7 @@ if (change == "F") {
     else {
         console.log(number + "C");
     }
-    if (-273.15 > number && number < 0) {
+    if (number < 0) {
         console.log("Temperatura poniżej zera");
     }
     if (number == 100) {
@@ -19,21 +20,22 @@ if (change == "F") {
         console.log("Temperatura zamarzania wody");
     }
 }
+
 if (change == "C") {
     number = (number * 9 / 5) + 32;
     if (number < -459.4) {
         console.log("Temperatura zera bezwglęgnego zmięń dane");
     }
     else {
-        console.log(Math.round(number) + "F");
+        if (number < 32) {
+            console.log("Temperatura poniżej zera");
+        }
+        if (number == 212) {
+            console.log("Temperatura wrzenia wody ");
+        }
+        if (number == 32) {
+            console.log("Temperatura zamarzania wody");
+        }
     }
-    if (-459.4 > number && number < 32) {
-        console.log("Temperatura poniżej zera");
-    }
-    if (number == 212) {
-        console.log("Temperatura wrzenia wody ");
-    }
-    if (number == 32) {
-        console.log("Temperatura zamarzania wody");
-    }
+    console.log(Math.floor(number) + "F");
 }
